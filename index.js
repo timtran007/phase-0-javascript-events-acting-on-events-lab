2813d9a37cdfa93097b9af4d4db3dc18a3b1eb32
Tim <timtran007@gmail.com>
2022-09-29 23:47:26 -0500

    commit tree 0af23ef8ebe3436f2bd0c80200f1bc904f68c2ff

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+const dodger = document.querySelector("#dodger")
+
+function moveDodgerLeft(){
+        let leftNumbers = dodger.style.left.replace("px", '')
+        let left = parseInt(leftNumbers, 0)
+        if(left > 0){
+        dodger.style.left = `${left - 10}px`
+        }
+    }
+
+
+function moveDodgerRight(){
+        let leftNumber = dodger.style.left.replace("px", "")
+        let left = parseInt(leftNumber, 0)
+        if(left < 360){
+            dodger.style.left = `${left + 10}px`
+        }
+}
+
+
+document.addEventListener('keydown', function(event){
+    if(event.key === "ArrowLeft"){
+        moveDodgerLeft()
+    } else if(event.key === "ArrowRight"){
+        moveDodgerRight()
+    }
+})
